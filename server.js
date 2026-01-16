994ae366141887631606fea021ff4e068c5ed96b
ham-zaza <hamzashahidqureshi9360@gmail.com>
2026-01-16 08:39:07 +0500

    Updated server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const httpServer = createServer(app);
 ======================= */
 const io = new Server(httpServer, {
     cors: {
-        origin: '*',
+        origin: '*', // (allowing all) just for the demo to prevent blocking errors
         methods: ['GET', 'POST']
     }
 });
